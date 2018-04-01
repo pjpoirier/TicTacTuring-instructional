8551bc385b438edcd8b5f220c14d742aa9600b2b
pjpoirier <34221989+pjpoirier@users.noreply.github.com>
2018-04-01 16:49:33 -0400

    First

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+	<div><h1>TicTacTuring</h1></div>
+	, document.getElementById('root'));
+registerServiceWorker();
